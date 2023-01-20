51eaf36b6c85db4cb5388e9a44bb436ae75b8c06
cipher-edu <ciphereduuz@gmail.com>
2023-01-20 22:15:23 +0500

    add dist

--- teachers/admin.py
+++ teachers/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
+class Teacheradmin(admin.ModelAdmin):
+    list_display = ('name')
 admin.site.register(Teacher)
