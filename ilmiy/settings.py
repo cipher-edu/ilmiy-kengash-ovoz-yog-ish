9963c1dba7aaa1abe2a75094268c76a2dd5cca5b
cipher-edu <ciphereduuz@gmail.com>
2025-06-25 12:32:34 +0500

    add

--- ilmiy/settings.py
+++ ilmiy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -77,23 +78,22 @@ WSGI_APPLICATION = 'ilmiy.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'baza.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql' if os.environ.get('POSTGRES_DB') else 'django.db.backends.sqlite3',
-#         'NAME': os.environ.get('POSTGRES_DB', BASE_DIR / 'baza.sqlite3'),
-#         'USER': os.environ.get('POSTGRES_USER', ''),
-#         'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
-#         'HOST': os.environ.get('POSTGRES_HOST', ''),
-#         'PORT': os.environ.get('POSTGRES_PORT', ''),
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'baza.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql' if os.environ.get('POSTGRES_DB') else 'django.db.backends.sqlite3',
+        'NAME': os.environ.get('POSTGRES_DB', BASE_DIR / 'baza.sqlite3'),
+        'USER': os.environ.get('POSTGRES_USER', ''),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', ''),
+        'HOST': os.environ.get('POSTGRES_HOST', ''),
+        'PORT': os.environ.get('POSTGRES_PORT', ''),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
